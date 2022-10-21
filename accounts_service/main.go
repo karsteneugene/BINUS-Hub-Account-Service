8225a6d091e0553569b0cc94b3abafeea48f0d9b
Ellyz Yaory <ellyz.yaory@binus.ac.id>
2022-10-21 07:43:30 +0700

    get course and class struct

--- accounts_service/main.go
+++ accounts_service/main.go
@@ -16,19 +16,44 @@ type account struct {
 	Role        string `json:"role"`
 }
 
+// Struct holding the course information
+type course struct {
+	ID string `json:"id"`
+	Course_Name string `json:"course_name"`
+}
+
+// Struct holding the class information
+type class struct {
+	ID string `json:"id"`
+	Class_Name string `json:"class_name"`
+}
+
 // Array holding account struct records
 var accounts = []account{{ID: "0", Binusian_ID: "2440035596", Fname: "Darren", Lname: "Pangesa", Email: "darren.pangesa@binus.ac.id", Role: "student"}}
+var courses = []course{{ID: "0", Course_Name: "Distributed System"}}
+var classes = []class{{ID: "0", Class_Name: "L4AC"}}
 
 // getAccounts responds with the list of all accounts in JSON format
 func getAccounts(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, accounts)
 }
 
+// getCourses responds with the list of all courses in JSON format
+func getCourses(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, courses)
+}
+
+// getClasses responds with the list of all classes in JSON format
+func getClasses(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, classes)
+}
+
 // Setting up an association between the function and endpoints
 func main() {
 	router := gin.Default()
 	router.GET("/accounts", getAccounts)
-
+	router.GET("/courses", getCourses)
+	router.GET("/classes", getClasses)
 	router.Run("localhost:8080")
 
 }

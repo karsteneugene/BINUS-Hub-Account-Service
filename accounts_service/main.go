22f9b854055e5d0a86e441200a5f9a474141db7b
Ellyz Yaory <ellyz.yaory@binus.ac.id>
2022-10-24 20:33:52 +0700

    Revised id primary key and password hash

--- accounts_service/main.go
+++ accounts_service/main.go
@@ -8,31 +8,32 @@ import (
 
 // Struct holding the account information
 type account struct {
-	ID          uint   `gorm: "primaryKey"`
-	Binusian_ID string `json:"binusian_id"`
-	Fname       string `json:"first_name"`
-	Lname       string `json:"last_name"`
-	Email       string `json:"email"`
-	Phone_No    string `json:"phone_no"`
-	Role        string `json:"role"`
-	Description string `json:"description"`
-	Profile_Img string `json:"profile_img"`
+	ID           uint   `json:"id" gorm:"column:id;PRIMARY_KEY"`
+	Binusian_ID  string `json:"binusian_id"`
+	Fname        string `json:"first_name"`
+	Lname        string `json:"last_name"`
+	Email        string `json:"email" gorm:"unique"`
+	PasswordHash string `json:"password_hash"`
+	Phone_No     string `json:"phone_no" gorm:"unique"`
+	Role         string `json:"role"`
+	Description  string `json:"description"`
+	Profile_Img  string `json:"profile_img"`
 }
 
 // Struct holding the course information
 type course struct {
-	ID          uint `gorm:"primaryKey"`
-	Course_Name string `json:"course_name"`
+	ID           uint   `json:"id" gorm:"column:id;PRIMARY_KEY"`
+	Course_Name  string `json:"course_name"`
 }
 
 // Struct holding the class information
 type class struct {
-	ID         uint   `gorm:"primaryKey"`
-	Class_Name string `json:"class_name"`
+	ID           uint   `json:"id" gorm:"column:id;PRIMARY_KEY"`
+	Class_Name   string `json:"class_name"`
 }
 
 // Array holding account struct records
-var accounts = []account{{ID: 0, Binusian_ID: "2440035596", Fname: "Darren", Lname: "Pangesa", Email: "darren.pangesa@binus.ac.id", Phone_No: "081219201007", Role: "student", Description: "Lorem ipsum", Profile_Img: "<path of image>"}}
+var accounts = []account{{ID: 0, Binusian_ID: "2440035596", Fname: "Darren", Lname: "Pangesa", Email: "darren.pangesa@binus.ac.id", PasswordHash: "", Phone_No: "081219201007", Role: "student", Description: "Lorem ipsum", Profile_Img: "<path of image>"}}
 var courses = []course{{ID: 0, Course_Name: "Distributed System"}}
 var classes = []class{{ID: 0, Class_Name: "L4AC"}}
 

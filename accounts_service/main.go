29ef7e1e0a0312a184423b71a9d5d3d1e4285f43
Ellyz Yaory <ellyz.yaory@binus.ac.id>
2022-10-24 20:15:26 +0700

    Revised using gorm primary key

--- accounts_service/main.go
+++ accounts_service/main.go
@@ -8,30 +8,33 @@ import (
 
 // Struct holding the account information
 type account struct {
-	ID          string `json:"id"`
+	ID          uint   `gorm: "primaryKey"`
 	Binusian_ID string `json:"binusian_id"`
 	Fname       string `json:"first_name"`
 	Lname       string `json:"last_name"`
 	Email       string `json:"email"`
+	Phone_No    string `json:"phone_no"`
 	Role        string `json:"role"`
+	Description string `json:"description"`
+	Profile_Img string `json:"profile_img"`
 }
 
 // Struct holding the course information
 type course struct {
-	ID string `json:"id"`
+	ID          uint `gorm:"primaryKey"`
 	Course_Name string `json:"course_name"`
 }
 
 // Struct holding the class information
 type class struct {
-	ID string `json:"id"`
+	ID         uint   `gorm:"primaryKey"`
 	Class_Name string `json:"class_name"`
 }
 
 // Array holding account struct records
-var accounts = []account{{ID: "0", Binusian_ID: "2440035596", Fname: "Darren", Lname: "Pangesa", Email: "darren.pangesa@binus.ac.id", Role: "student"}}
-var courses = []course{{ID: "0", Course_Name: "Distributed System"}}
-var classes = []class{{ID: "0", Class_Name: "L4AC"}}
+var accounts = []account{{ID: 0, Binusian_ID: "2440035596", Fname: "Darren", Lname: "Pangesa", Email: "darren.pangesa@binus.ac.id", Phone_No: "081219201007", Role: "student", Description: "Lorem ipsum", Profile_Img: "<path of image>"}}
+var courses = []course{{ID: 0, Course_Name: "Distributed System"}}
+var classes = []class{{ID: 0, Class_Name: "L4AC"}}
 
 // getAccounts responds with the list of all accounts in JSON format
 func getAccounts(c *gin.Context) {

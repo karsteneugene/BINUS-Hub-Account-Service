a9ddd24438486516a2dc3f382d4ffd09f85319ee
DarrenMannuela <darren.mannuela@gmail.com>
2022-11-18 17:26:20 +0700

    handler for course updated

--- API/v1/courses_handler.go
+++ API/v1/courses_handler.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-var courses = []model.Course{{Course_ID: "0", Course_Name: "Distributed System"}}
+var courses = []model.Course{{Course_ID: "0", Course_Name: "Distributed System", Class_ID: "L1AC", Lecturer_ID: "D6059"}}
 
 func GetCourses(c *fiber.Ctx) error {
 	return c.JSON(courses)
@@ -19,6 +19,8 @@ func SetCourses(c *fiber.Ctx) error {
 
 	NewCourse.Course_ID = "1"
 	NewCourse.Course_Name = "Intelligent Systems"
+	NewCourse.Class_ID = "Intelligent Systems"
+	NewCourse.Lecturer_ID = "D6058"
 
 	courses = append(courses, *NewCourse)
 	return c.JSON(NewCourse)

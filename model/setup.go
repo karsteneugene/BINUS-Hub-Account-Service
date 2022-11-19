9d693a98524f7da5f5e7a308861552e1bc59ed8e
DarrenMannuela <darren.mannuela@gmail.com>
2022-11-19 16:37:01 +0700

    added credentials for localhost db

--- model/setup.go
+++ model/setup.go
@@ -14,8 +14,11 @@ var dbConn *gorm.DB
 // Connect : Create a database sql connection
 func Connect() {
 
-	// Darren root:Scorch120403
+	// Karsten root:DSAccounts#2024
 	db, err := gorm.Open(mysql.Open("root:DSAccounts#2024@tcp(127.0.0.1:3306)/accounts_svc"))
+
+	// Darren root:Scorch120403
+	//db, err := gorm.Open(mysql.Open("root:Scorch120403@tcp(127.0.0.1:3306)/accounts_svc"))
 
 	// ellyz dbadmin:password
 	//db, err := gorm.Open(mysql.Open("dbadmin:password@tcp(127.0.0.1:3306)/accounts_svc"))
